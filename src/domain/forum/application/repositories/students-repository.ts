58bd0e5fa386cd4be1e0a28db28bf1a1464a322e
Hugo Oliveira <hugooliveira.dev@proton.me>
2025-10-28 23:55:46 -0300

    feat: add students repository and hasher intefaces

---
+++ src/domain/forum/application/repositories/students-repository.ts
@@ -0,0 +1,7 @@
+import { Student } from '@/domain/forum/enterprise/entities/student'
+
+export abstract class StudentsRepository {
+  abstract findById(id: string): Promise<Student | null>
+  abstract findByEmail(email: string): Promise<Student | null>
+  abstract create(student: Student): Promise<void>
+}
